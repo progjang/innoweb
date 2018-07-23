c24486c7618d3950290d801ccd7110c69112949a
S Y Chang <wooritobi@gmail.com>
2018-07-23 10:28:53 +0900

    settings/wsgi.py에서 sys.path에 django 경로를 추가

--- settings/wsgi.py
+++ settings/wsgi.py
@@ -7,7 +7,9 @@ For more information on this file, see
 https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
+
+sys.path.append('/home/ubuntu/.local/lib/python3.5/site-packages')
 
 from django.core.wsgi import get_wsgi_application
 
