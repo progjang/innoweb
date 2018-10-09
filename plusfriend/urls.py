fd942ec5b3489ee72e8884605f2e29c9a319fc14
Prog Chang <wooritobi@gmail.com>
2018-10-09 20:38:28 +0900

    plusfriend 앱에서 urls.py 오류수정

--- plusfriend/urls.py
+++ plusfriend/urls.py
@@ -4,10 +4,10 @@ from . import views
 app_name = 'plusfriend'
 
 urlpatterns = [
-    path('^friend$', views.on_added),
-    path('^keyboard$', views.on_init),
-    path('^friend/<user_key>$', views.on_block),
-    path('^chat_room/<user_key>$', views.on_leave),
-    path('^message$', views.on_message),
-    path('^diary/$', views.post_list, name='post_list'),
+    path('friend', views.on_added),
+    path('keyboard', views.on_init),
+    path('friend/<user_key>', views.on_block),
+    path('chat_room/<user_key>', views.on_leave),
+    path('message', views.on_message),
+    path('diary', views.post_list, name='post_list'),
 ]
